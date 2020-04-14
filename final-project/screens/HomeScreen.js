4a8b9c0c6c5dcc8c05db24e4fd44e689b338ae2b
trevor.patrick <40744678+trevor-patrick@users.noreply.github.com>
2020-04-13 20:39:01 -0400

    add game to DB

--- final-project/screens/HomeScreen.js
+++ final-project/screens/HomeScreen.js
@@ -1,24 +1,41 @@
 import firebaseApp from '../firebase.js';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button, Alert } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button, Alert, TextInput } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { MonoText } from '../components/StyledText';
 
 var database = firebaseApp.database();
 
-export default function HomeScreen() {
 
+
+export default function HomeScreen() {
+  const [gameNameInput, setGameNameInput] = React.useState(null)
+  const [keyInput, setKeyInput] = React.useState(null)
   return (
     <View style={styles.container}>
       {/* <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}> */}
 
-      {/* <View style={styles.button}> */}
-      <Button
-        onPress={() => firebaseTest()}
-        title="Test button for Firebase"
-      />
-      {/* </View> */}
+      <View style={styles.form}>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Game name"
+          onChangeText={(text) => setGameNameInput(text)}>
+        </TextInput>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Key"
+          onChangeText={(text) => setKeyInput(text)}
+        >
+        </TextInput>
+
+        <TouchableOpacity style={styles.button} onPress={() => addGameToDatabase(gameNameInput, keyInput)}>
+          <Text style={styles.buttonText}>Add game to database</Text>
+        </TouchableOpacity>
+
+      </View>
 
       {/* </ScrollView> */}
 
@@ -26,14 +43,14 @@ export default function HomeScreen() {
   );
 }
 
-function firebaseTest() {
-  var ref = database.ref("users");
+function addGameToDatabase(gameName, key) {
+  console.log(gameName);
+  console.log(key);
+  var ref = database.ref("games");
 
-  ref.set({
-    trevor: {
-      date_of_birth: "06/01/1998",
-      timestamp: Date.now()
-    }
+  ref.push({
+    gameName: gameName,
+    key: key
   })
 }
 
@@ -70,12 +87,26 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
   button: {
-    // backgroundColor: "#FFD700",
+    backgroundColor: "lightblue",
     padding: 4,
     marginBottom: 10,
     width: "100%",
-    borderRadius: 200,
-    overflow: 'hidden',
-    // height: "5%"
+    // borderRadius: 10,
+    // overflow: 'hidden'
   },
+  buttonText: {
+    fontSize: 18,
+    textAlign: "center"
+  },
+  input: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 200,
+    overflow: 'hidden'
+  },
+  form: {
+    width: "15%"
+  }
 });
